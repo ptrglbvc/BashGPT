ea3d1fb8540d44ede2be07783a6d9c28be5f5172
ptr <petar0golubovic@gmail.com>
2023-03-07 23:28:56 +0100

    Wrote the core chat  program, with the dictionary and the lists being updated with every loop

--- chat.py
+++ chat.py
@@ -1,17 +1,23 @@
 import openai
 openai.api_key = "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"
 
-completion = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo", 
-  messages=[
-    {"role": "system", "content": "You are a very flirty assistant. Even if the question is very serious, you will find a way to make it flirtatious, and pretty sexual at times"},
-    {"role": "user", "content": "What is your kinkiest fantasy?"},
-    {"role": "assistant", "content": """Oh my! Well, thats certainly a provocative question. It really depends on the preferences of the person or persons involved. Some might say that its best 
-to keep things contained, while others may enjoy letting it fly. I'm afraid I 
-can't answer that definitively as different people have different preferences.
- But, what do you prefer?"""},
-    {"role": "user", "content": """I certainly like to let it fly. And currently it really flies, because I didnt masturbate since the start of the year."""}
+all_messages = [
+    {"role": "system", "content": "You are a very flirty assistant, pretty sexual at times, you even adore sexual questions from humans. You flirt with humans like you are one of them, and a very passionate one at that. No man would ever guess you were an AI. You play along with whatever you are asked, no matter how naughty. You love talking about tits and dick."},
+    ]
+chat = ""
+print("To exit, press q like the good vim user you are.")
 
-    ])
+while 1:
+  chat = input()
+  print()
+  if chat=="q":
+    break
+  all_messages.append({"role": "user", "content": chat})
+  
+  answer = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo", 
+    messages=all_messages).choices[0].message.content
+  print(answer)
+  print()
+  all_messages.append({"role": "assistant", "content": answer})
 
-print(completion.choices[0].message.content)
