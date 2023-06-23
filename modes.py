97265266a1589f264aefa36f55b02a0e7eadf3cb
ptr <petar0golubovic@gmail.com>
2023-06-23 12:49:46 +0200

    removed the bash_mode.txt file, as it's not needed anymore

--- modes.py
+++ modes.py
@@ -4,7 +4,7 @@ import json
 path = str(Path(__file__).parent.resolve()) + "/"
 jason_path = path + "modes.json"
 
-# short mode is the default mode, so it doesn't need to be loaded into modes
+# short mode is the default mode, it has to exist, even if the json is deleted 
 short_mode = ("Answer every question you get as shortly and as matter of factly "
               "as possible. Just focus on the answer to the question and nothing else. "
               "No fat. For example: Who won the premier league? - Manchester City. "
