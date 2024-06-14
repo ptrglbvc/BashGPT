6db3a03f003692750c0c399ee75f6f193601ca56
ptr <petar0golubovic@gmail.com>
2024-06-14 18:37:12 +0200

    modified the load_defaults function so that it checks is the defaults.json exists before opening it. If not, it creates it.

--- src/bashgpt/load_defaults.py
+++ src/bashgpt/load_defaults.py
@@ -1,9 +1,14 @@
 import json
 from pathlib import Path
+from os.path import exists
 
 defaults_path = str(Path(__file__).parent.resolve()) + "/defaults.json"
 
 def load_defaults():
+    if not exists(defaults_path):
+        open(defaults_path, 'a').close()
+        
+    
     with open(defaults_path, "r") as defaults:
         if defaults:=defaults.read().strip():
             return json.loads(defaults)
